eca3487fb77c2f4a7093c2bd511429da7440b933
Alem93 <Alem93@users.noreply.github.com>
2017-04-13 23:42:12 -0300

    Added Pokemon class

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Alejandro Mamchur on 4/13/17.
+//  Copyright © 2017 Alejandro Mamchur. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedex: Int) {
+        self._name = name
+        self._pokedexId = pokedex
+    }
+}
